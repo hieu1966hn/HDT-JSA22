12288cfa3904c21892e893a04032d2b6e850a4b7
hieu1966hn <hieu1966hn@gmail.com>
2022-07-08 19:58:29 +0700

    new code Chữa bài 1

---
+++ SS6/Chữa_BT_checkpoint1/app.js
@@ -0,0 +1,14 @@
+///// Bài 1
+// function findEven(number) {
+//   if (number >= 4 && number <= 20) { // &&: Chỉ trả về true khi các điều kiện tương ứng đều true. Còn lại trả về false
+//     for (let i = 0; i <= number; i = i + 2) {
+//       console.log(i);
+//     }
+
+
+//   }
+// }
+
+// findEven(6); // 0, 2,4,6
+
+
