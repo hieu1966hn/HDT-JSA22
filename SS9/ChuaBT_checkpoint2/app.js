5582196820174808873c10d783fcc7ab3891bb49
hieu1966hn <hieu1966hn@gmail.com>
2022-07-22 20:02:07 +0700

    Done Bài 02

--- SS9/ChuaBT_checkpoint2/app.js
+++ SS9/ChuaBT_checkpoint2/app.js
@@ -2,15 +2,39 @@
 
 ////// Bài 1:
 
-function student(name, age, address) {
+// function student(name, age, address) {
 
-  return {
-    "name": name,
-    // khóa : giá trị
-    "age": age,
-    "address": address,
+//   return {
+//     "name": name,
+//     // khóa : giá trị
+//     "age": age,
+//     "address": address,
+//   }
+
+// }
+
+// console.log(student("Hiếu", 25, "Hà Nội"));
+
+
+//// Bài 02: 
+
+let smartPhones = [
+  { name: 'iphone', price: 649 },
+  { name: 'Galaxy S6', price: 576 },
+  { name: 'Galaxy Note 5', price: 489 }
+];
+
+function returnPrice() { // từ đầu tiên của chữ cái thứ 2 phải được viết hoa (Quy tắc đặt tên: camel case)
+  //// C1
+  for (let key of smartPhones) { // for .key.. of:  Hiểu là đại diện cho từng phần tử của mảng
+    console.log(`${key.name}: `, key.price);
   }
 
+  //// C2
+  // for (let i = 0; i < smartPhones.length; i++) {
+  //   console.log(smartPhones[i].price);
+  // }
 }
 
-console.log(student("Hiếu", 25, "Hà Nội"));
+
+returnPrice();
