95ce3a0e88c2b3d75821f276a78e009d10f0a913
hieu1966hn <hieu1966hn@gmail.com>
2022-07-22 19:54:42 +0700

    done chữa bài 01

---
+++ SS9/ChuaBT_checkpoint2/app.js
@@ -0,0 +1,16 @@
+/// chữa Bài tập checkpoint 2
+
+////// Bài 1:
+
+function student(name, age, address) {
+
+  return {
+    "name": name,
+    // khóa : giá trị
+    "age": age,
+    "address": address,
+  }
+
+}
+
+console.log(student("Hiếu", 25, "Hà Nội"));
