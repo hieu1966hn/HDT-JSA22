290c357b98aca295a2acce506170669a911199f8
hieu1966hn <hieu1966hn@gmail.com>
2022-07-22 20:08:58 +0700

    done bài 03

--- SS9/ChuaBT_checkpoint2/app.js
+++ SS9/ChuaBT_checkpoint2/app.js
@@ -18,23 +18,46 @@
 
 //// Bài 02: 
 
-let smartPhones = [
-  { name: 'iphone', price: 649 },
-  { name: 'Galaxy S6', price: 576 },
-  { name: 'Galaxy Note 5', price: 489 }
-];
+// let smartPhones = [
+//   { name: 'iphone', price: 649 },
+//   { name: 'Galaxy S6', price: 576 },
+//   { name: 'Galaxy Note 5', price: 489 }
+// ];
 
-function returnPrice() { // từ đầu tiên của chữ cái thứ 2 phải được viết hoa (Quy tắc đặt tên: camel case)
-  //// C1
-  for (let key of smartPhones) { // for .key.. of:  Hiểu là đại diện cho từng phần tử của mảng
-    console.log(`${key.name}: `, key.price);
-  }
+// function returnPrice() { // từ đầu tiên của chữ cái thứ 2 phải được viết hoa (Quy tắc đặt tên: camel case)
+//   //// C1
+//   for (let key of smartPhones) { // for .key.. of:  Hiểu là đại diện cho từng phần tử của mảng
+//     console.log(`${key.name}: `, key.price);
+//   }
 
-  //// C2
-  // for (let i = 0; i < smartPhones.length; i++) {
-  //   console.log(smartPhones[i].price);
-  // }
+//   //// C2
+//   // for (let i = 0; i < smartPhones.length; i++) {
+//   //   console.log(smartPhones[i].price);
+//   // }
+// }
+
+
+// returnPrice();
+
+
+///// Bài 03; Chuyển các hàm dưới đây => ES6 (Arrow Function)
+function foo1(x, y, z) {
+  console.log(x, y, z);
 }
 
+const foo_1 = (x, y, z) => {
+  console.log(x, y, z);
+}
 
-returnPrice();
+foo_1(1, 2, 3)
+// => Biến foo_1 chứa hàm. Dô được khai báo với cú pháp "const" => foo_1 là 1 hàm vĩnh cửu
+
+/////Tương tự với các hàm dưới
+// --------------------------
+function foo2() {
+  alert("Hello")
+}
+function foo3(a, b) {
+  let m = a + b * 100
+  return m;
+}
